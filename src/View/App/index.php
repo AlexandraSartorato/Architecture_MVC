edd41e107fb56427ab8aee75eb782d18d90e8d27
alexoukimchouette <32076774+alexoukimchouette@users.noreply.github.com>
2018-03-06 13:35:48 +0100

    design update

--- src/View/App/index.php
+++ src/View/App/index.php
@@ -1,11 +1,23 @@
 <div class="parallax-container">
     <div class="parallax"><img src="https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/06/fut_wot_keyart-h_2017.jpg"></div>
-    <a class="waves-effect waves-light btn">Sign up</a>
 </div>
+<a class="waves-effect waves-light btn center-align">Sign up</a>
 <div class="section white">
     <div class="row container">
-        <div class="col s6">
-        <p class="grey-text text-darken-3 lighten-3">My Movies List</p>
+        <div class="col s4">
+            <h4 class="grey-text text-darken-3 lighten-3 center-align">Update information</h4>
+            <p class="grey-text text-darken-3 lighten-3 center-align"><i class="fas fa-wrench"></i></p>
+            <p class="grey-text text-darken-3 lighten-3 center-align">As a member of this community you have the possibily to update the informations</p>
+        </div>
+        <div class="col s4">
+            <h4 class="grey-text text-darken-3 lighten-3 center-align">Community share</h4>
+            <p class="grey-text text-darken-3 lighten-3 center-align"><i class="fas fa-share-alt"></i></p>
+            <p class="grey-text text-darken-3 lighten-3 center-align">With this community, you can share what you watched and  easely find your favorites movies.</p>
+        </div>
+        <div class="col s4">
+            <h4 class="grey-text text-darken-3 lighten-3 center-align">My Movies</h4>
+            <p class="grey-text text-darken-3 lighten-3 center-align"><i class="fas fa-film"></i></p>
+            <p class="grey-text text-darken-3 lighten-3 center-align">Sharing community movies</p>
         </div>
     </div>
 </div>
--- src/View/index.php
+++ src/View/index.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8" /> <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <title>Pie PHP</title>
